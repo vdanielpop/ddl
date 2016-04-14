dc3203eec8deb0a83e05cf91a1c6c3435c6d1cf0
Daniel Vasile Pop <vpop@pitechnologies.ro>
2016-04-14 19:12:02 +0300

    Changed static service calls to DI, added serialization fallback & improved docblocks. See https://www.drupal.org/node/2705731 .

--- src/Controller/DevelDebugLogController.php
+++ src/Controller/DevelDebugLogController.php
@@ -4,6 +4,9 @@ namespace Drupal\devel_debug_log\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Database\Connection;
+use Drupal\Core\Datetime\DateFormatter;
+use Drupal\Core\Form\FormBuilderInterface;
+use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class DevelDebugLogController extends ControllerBase {
@@ -16,23 +19,59 @@ class DevelDebugLogController extends ControllerBase {
   protected $database;
 
   /**
+   * The Serializer service.
+   *
+   * @var \Drupal\rest\Plugin\views\style\Serializer
+   */
+  protected $serializer;
+
+  /**
+   * The DateFormatter service.
+   *
+   * @var \Drupal\Core\Datetime\DateFormatter
+   */
+  protected $dateFormatter;
+
+  /**
+   * The FormBuilder object.
+   *
+   * @var Drupal\Core\Form\FormBuilderInterface;
+   */
+  protected $formBuilder;
+
+  /**
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container) {
     return new static(
-      $container->get('database')
+      $container->get('database'),
+      $container->get('date.formatter'),
+      $container->get('serializer'),
+      $container->get('form_builder')
     );
   }
 
   /**
    * DevelDebugLogController constructor.
    *
-   * @param \Drupal\Core\Database\Connection $database
+   * @param Connection $database
+   * @param DateFormatter $dateFormatter
+   * @param Serializer $serializer
+   * @param FormBuilderInterface $formBuilder
    */
-  public function __construct(Connection $database) {
+  public function __construct(Connection $database, DateFormatter $dateFormatter, Serializer $serializer, FormBuilderInterface $formBuilder) {
     $this->database = $database;
+    $this->dateFormatter = $dateFormatter;
+    $this->serializer = $serializer;
+    $this->formBuilder = $formBuilder;
   }
 
+  /**
+   * Lists debug information.
+   *
+   * @return array
+   *  Renderable array that contains a list of debug data.
+   */
   public function listLogs() {
     $query = $this->database->select('devel_debug_log', 'm')
       ->extend('Drupal\Core\Database\Query\PagerSelectExtender');
@@ -43,12 +82,12 @@ class DevelDebugLogController extends ControllerBase {
     $rows = [];
     foreach ($results as $result) {
       if ($result->serialized) {
-        $result->message = unserialize($result->message);
+        $result->message = $this->serializer->unserialize($result->message);
       }
 
       $rows[] = array(
         'title' => $result->title,
-        'time' => \Drupal::service('date.formatter')
+        'time' => $this->dateFormatter
             ->format($result->timestamp, 'short'),
         'message' => $this->ob_kint($result->message),
       );
@@ -64,7 +103,7 @@ class DevelDebugLogController extends ControllerBase {
       'messages' => array(
         '#theme' => 'devel_debug_log_list',
         '#content' => $rows,
-        '#delete_form' => \Drupal::formBuilder()->getForm('Drupal\devel_debug_log\Form\DevelDebugLogDeleteForm'),
+        '#delete_form' => $this->formBuilder->getForm('Drupal\devel_debug_log\Form\DevelDebugLogDeleteForm'),
       ),
       'pager' => array(
         '#type' => 'pager'
@@ -74,6 +113,19 @@ class DevelDebugLogController extends ControllerBase {
     return $build;
   }
 
+  /**
+   * Provides debug output for later printing.
+   *
+   *  Usually, kint() outputs the debug information as the first thing after the
+   * <body> tag. This function allows you to get that output and use it for
+   * later printing.
+   *
+   * @param mixed $message
+   *  The data that's displayed for debugging.
+   *
+   * @return string
+   *  The debug information.
+   */
   private function ob_kint($message) {
     ob_start();
     kint($message);
